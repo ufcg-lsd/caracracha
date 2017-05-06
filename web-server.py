96c812c18a48a9909bf929a6fa0dbfc544d242ab
Pedro Barbosa <pedroyossis@copin.ufcg.edu.br>
2017-05-06 11:35:58 -0300

    added ip in index.html

--- web-server.py
+++ web-server.py
@@ -1,10 +1,13 @@
 import json
 import requests
+import sched, time
 from flask import Flask, flash, request, render_template, session
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
+s_macs = sched.scheduler(time.time, time.sleep)
+previousMacs = []
 
 class RegistrationForm(Form):
     name = StringField('Name', [validators.Length(min=4, max=25)])
@@ -24,25 +27,39 @@ def index():
     try:
         form = RegistrationForm(request.form)
         form.msg = ''
-        with open('users.json') as f:
-            registered_users = json.load(f)
+        with open('macs.json') as f:
+            registered_macs = json.load(f)
         
         if request.method == 'POST' and form.validate():
-            registered_users[form.name.data] = form.mac.data
-            with open('users.json', 'w') as f:
-                f.write(json.dumps(registered_users))
+            registered_macs[form.mac.data] = form.name.data
+            with open('macs.json', 'w') as f:
+                f.write(json.dumps(registered_macs))
             form.msg = 'Thanks for registering :)'
         
         #TODO: make API request to present only the present users.
         form.users = []
-        for user in registered_users:
-            form.users.append(user)
+        for mac in previousMacs:
+            form.users.append(registered_macs[mac])
 
         return render_template('index.html', form=form)
     except Exception as e:
         print(e)
 
+def update_users():
+    r = requests.get("http://150.165.85.12:41205/get_number_of_macs?identifier=LSD&m=2")
+    macs = json.loads(r.text)["Macs"]
+    currentMacs = []
+    for mac in macs:
+        if mac not in previousMacs:
+            currentMacs.append(mac)
+    previousMacs = currentMacs
+    s_macs.enter(60, 1, update_users, (s_macs,))
+
 if __name__ == '__main__':
     #TODO: create assynchronous function to monitor when a new user appears
+   
     app.debug = True
     app.run("0.0.0.0", 80)
+    s_macs.enter(60, 1, update_users, (s_macs,))
+    s_macs.run()
+ 
